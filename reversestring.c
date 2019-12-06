5c823fe189f850598f2706436f5aa702c8aa2e87
uzma29 <48977018+uzma29@users.noreply.github.com>
2019-12-06 21:38:41 +0530

    Create reversestring.c

---
+++ reversestring.c
@@ -0,0 +1,30 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+   char s[100], r[100];
+   int begin, end, count = 0;
+
+   printf("Input a string\n");
+   gets(s);
+
+   // Calculating string length
+
+   while (s[count] != '\0')
+      count++;
+
+   end = count - 1;
+
+   for (begin = 0; begin < count; begin++) {
+      r[begin] = s[end];
+      end--;
+   }
+
+   r[begin] = '\0';
+
+   printf("The reversed string is %s\n", r);
+
+   return 0;
+}
